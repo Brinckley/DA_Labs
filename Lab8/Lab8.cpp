4180c7794478828975af0d8913f4f13a0d49d691
Brinckley <57827932+Brinckley@users.noreply.github.com>
2021-10-13 13:07:17 +0300

    lab8 code rewritten

--- Lab8/Lab8.cpp
+++ Lab8/Lab8.cpp
@@ -14,43 +14,61 @@ bool operator==(const TSegment &lhs, TSegment &rhs) {
 }
 
 int main() { 
-    long long n, m;
+    long long n = 0, m = 0;
     long long globalLeft = 0;
     long long globalRight = 0;
+    bool flag = true;
     std::cin >> n;
+    if(n == 0) {
+        std::cout << 0 << std::endl;
+        return 0;
+    }
+
     std::vector<TSegment> list(n);
     std::vector<TSegment> answer;
+    std::vector<TSegment> start;
 
     for(int i = 0; i < n; ++i) {
         std::cin >> list[i].left >> list[i].right;
         list[i].number = i;
     }
     std::cin >> m;
+    if(m < 0) {
+        std::cout << 0 << std::endl;
+        return 0;
+    }
     
+    start = list;
     std::sort(list.begin(), list.end(),
     [](const TSegment v1, const TSegment v2) 
     { return (v1.left < v2.left); });
 
-
-    for(std::vector<TSegment>::iterator it = list.begin(); it != list.end() && globalLeft < m; it++) {
-        if(globalLeft >= it->right ) continue;
-
-        std::vector<TSegment>::iterator tmp = it;
-
-        while( globalLeft >= it->left && it != list.end() ) {
-            if( it->right >= globalRight ) {
-                globalRight = it->right, tmp = it;
+    int i = -1;
+    while(globalLeft < m && flag) {
+        int istart = i + 1;
+        globalRight = globalLeft;
+        for(int j = i + 1; j < n; ++j) {
+            if(list[j].left > globalLeft) {
+                break;
             }
-            it++;
+            if(list[j].right > globalRight) {
+                i = j;
+                globalRight = list[i].right;
+            }
         }
-
-        answer.push_back(*tmp);
-        globalLeft = globalRight;
-        if(globalLeft >= it->left ) {
-            it--;
+        if(i == istart - 1) {
+            flag = false;
+        }
+        else {
+            globalLeft = list[i].right;
+            for(int j = 0; j < n; ++j) {
+                if(list[i].left == start[j].left && list[i].right == start[j].right) {
+                    answer.push_back(start[j]);
+                    break;
+                }
+            }
         }
     }
-
     if(globalLeft >= m) {
         std::cout << answer.size() << std::endl;
 
